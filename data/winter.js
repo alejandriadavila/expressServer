90fc348b5bbb1aed0e54fb09051bec959854aae3
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2024-01-23 18:17:17 -0500

    Seasonal crop data added. Routes for each season started, but not finished.

---
+++ data/winter.js
@@ -0,0 +1,37 @@
+const winter = [
+    {
+        id: 1,
+        name: "Cotton",
+        type: "Resource",
+        seedName: "Cotton seeds",
+        seedCost: 50,
+        growthTime: 9,
+        regrowBoolean: F,
+        regrowTime: "Does not regrow.",
+        imgUrl: "https://static.wikia.nocookie.net/coralisland/images/c/c3/Cotton.png/revision/latest?cb=20221016022946",
+    },
+    {
+        id: 2,
+        name: "Snowdrop",
+        type: "Flower",
+        seedName: "Snowdrop seeds",
+        seedCost: 45,
+        growthTime: 12,
+        regrowBoolean: F,
+        regrowTime: "Does not regrow.",
+        imgUrl: "https://static.wikia.nocookie.net/coralisland/images/5/5c/Snowdrop.png/revision/latest?cb=20221020233512",
+    },
+    {
+        id: 3,
+        name: "Tea leaf",
+        type: "Vegetable",
+        seedName: "Tea seeds",
+        seedCost: 25,
+        growthTime: 11,
+        regrowBoolean: F,
+        regrowTime: "Does not regrow.",
+        imgUrl: "https://static.wikia.nocookie.net/coralisland/images/5/5d/Tea_leaf.png/revision/latest?cb=20221016023415",
+    }
+]
+
+module.exports = winter
